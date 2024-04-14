7229d24c614677d6961c5e0a1aca7c647868a587
Henrique <henrique1989@gmail.com>
2024-04-14 21:28:50 +0200

    Created the Users module

--- src/app/shared/materials/material/users-material.module.ts
+++ src/app/shared/materials/material/users-material.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [],
   providers: [],
   exports: [
     MatToolbarModule,
+    MatTabsModule,
     MatIconModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
+    MatButtonModule
   ]
 })
-export class MaterialModule { }
+export class UsersMaterialModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarTitleComponent } from './components/toolbar-title/toolbar-title.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+
+
+@NgModule({
+  declarations: [
+    ToolbarTitleComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    FlexLayoutModule
+  ],
+  exports: [
+    ToolbarTitleComponent,
+  ]
+})
+export class SharedModule { }
